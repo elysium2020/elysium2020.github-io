544f0ce46222f47db56dbae53f746310695fffcc
elysium2020 <elysium2020@users.noreply.github.com>
2025-03-26 18:00:52 +0800

    feat: more pretty tag and post

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,10 @@
 
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import eslintPluginAstro from 'eslint-plugin-astro';
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.recommended,
+  eslintPluginAstro.configs.recommended,
 );
